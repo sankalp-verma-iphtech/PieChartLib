f6118318feefc8bb3971d5d8af8961953dccc5de
sankalpverma <sankalp.verma@iphtechnologies.com>
2024-12-30 12:01:51 +0530

    Added pie chart cell

--- Sources/PieChartLibrary/PieChartLibrary.swift
+++ Sources/PieChartLibrary/PieChartLibrary.swift
@@ -1,2 +1,64 @@
+
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import SwiftUI
+
+struct PieSlice: Identifiable {
+    var id = UUID()
+    var startDeg: Double
+    var endDeg: Double
+    var value: Double
+    var normalizedValue: Double
+}
+
+@available(iOS 13.0, *)
+public struct PieChartCell : View {
+    @State private var show:Bool = false
+    var rect: CGRect
+    var radius: CGFloat {
+        return min(rect.width, rect.height)/2
+    }
+    var startDeg: Double
+    var endDeg: Double
+    var path: Path {
+        var path = Path()
+        path.addArc(center:rect.mid , radius:self.radius, startAngle: Angle(degrees: self.startDeg), endAngle: Angle(degrees: self.endDeg), clockwise: false)
+        path.addLine(to: rect.mid)
+        path.closeSubpath()
+        return path
+    }
+    var index: Int
+    var backgroundColor:Color
+    var accentColor:Color
+    public var body: some View {
+        path
+            .fill()
+            .foregroundColor(self.accentColor)
+            .overlay(path.stroke(self.backgroundColor, lineWidth: 2))
+            .scaleEffect(self.show ? 1 : 0)
+            .animation(Animation.spring().delay(Double(self.index) * 0.04))
+            .onAppear(){
+                self.show = true
+        }
+    }
+}
+
+extension CGRect {
+    var mid: CGPoint {
+        return CGPoint(x:self.midX, y: self.midY)
+    }
+}
+
+#if DEBUG
+struct PieChartCell_Previews : PreviewProvider {
+    @available(iOS 13.0, *)
+    static var previews: some View {
+        GeometryReader { geometry in
+            PieChartCell(rect: geometry.frame(in: .local),startDeg: 0.0,endDeg: 90.0, index: 0, backgroundColor: Color(red: 252.0/255.0, green: 236.0/255.0, blue: 234.0/255.0), accentColor: Color(red: 225.0/255.0, green: 97.0/255.0, blue: 76.0/255.0))
+            }.frame(width:100, height:100)
+        
+    }
+}
+#endif
